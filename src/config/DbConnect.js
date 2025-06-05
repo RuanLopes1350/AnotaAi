22b92c27d2c067c876522b63f69cbe8dc3ba471d
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-06-05 14:31:00 -0400

    adicionado logs

--- src/config/DbConnect.js
+++ src/config/DbConnect.js
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import Logger from '../utils/helpers/logger.js';
 
 dotenv.config();
 
 class DbConnect {
     static async conectar() {
+        const idOperacao = Logger.iniciarOperacao('conexão-db', 'Conectando ao banco de dados');
+        
         try {
             const mongoURI = process.env.DB_URL;
 
@@ -16,25 +19,36 @@ class DbConnect {
 
             mongoose.connection.on('connected', () => {
                 console.log('Conectado ao MongoDB');
+                Logger.info(`[${idOperacao}] Conexão estabelecida com o MongoDB`)
             })
 
             mongoose.connection.on('error', (erro) => {
                 console.error('Erro ao conectar ao MongoDB:', erro);
+                Logger.registrarErro(idOperacao, 'conexão-db', erro);
             });
 
             mongoose.connection.on('disconnected', () => {
                 console.log('Desconectado do MongoDB');
+                Logger.info(`[${idOperacao}] Desconexão do MongoDB`);
             });
+            
+            Logger.finalizarOperacao(idOperacao, 'conexão-db', 'Conexão estabelecida com sucesso');
+            
         } catch (erro) {
+            Logger.registrarErro(idOperacao, 'conexão-db', erro);
             throw new Error(`Erro ao conectar ao banco de dados: ${erro.message}`);
         }
     }
 
     static async desconectar() {
+        const idOperacao = Logger.iniciarOperacao('desconexão-db', 'Desconectando do banco de dados');
+        
         try {
             await mongoose.disconnect();
             console.log('Conexão com o MongoDB encerrada');
+            Logger.finalizarOperacao(idOperacao, 'desconexão-db', 'Conexão com o MongoDB encerrada com sucesso');
         } catch (erro) {
+            Logger.registrarErro(idOperacao, 'desconexão-db', erro);
             throw new Error(`Erro ao desconectar do banco de dados: ${erro.message}`);
         }
     }
