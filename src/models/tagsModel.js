f9ed7d4210a6aa8be89b1d1d93b46469af5ad48d
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-06-08 22:43:37 -0400

    adicionado models para tags

---
+++ src/models/tagsModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+class Tags {
+    constructor() {
+        const tagsSchema = new mongoose.Schema(
+            {
+                nome: { 
+                    type: String, 
+                    required: true, 
+                    index: true 
+                },
+                cor: { 
+                    type: String, 
+                    default: '#6c757d' 
+                },
+                usuarioId: {
+                    type: mongoose.Schema.Types.ObjectId, 
+                    ref: 'usuario', 
+                    required: true
+                }
+            },
+            {
+                timestamps: true
+            }
+        )
+
+        tagsSchema.index({ nome: 1, usuarioId: 1}, { unique: true }); // garante que não haja tags duplicadas para o mesmo usuário
+    }
+}
+
+export default new Tags().model;
